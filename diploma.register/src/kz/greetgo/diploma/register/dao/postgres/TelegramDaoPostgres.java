784b759e6e389ba521fc096b1129ba0ebe159434
flinkcstou <flinkcstou@gmail.com>
2019-03-14 00:30:31 +0600

    telegramBot

---
+++ diploma.register/src/kz/greetgo/diploma/register/dao/postgres/TelegramDaoPostgres.java
@@ -0,0 +1,8 @@
+package kz.greetgo.diploma.register.dao.postgres;
+
+import kz.greetgo.depinject.core.Bean;
+import kz.greetgo.diploma.register.dao.AuthDao;
+
+
+@Bean
+public interface TelegramDaoPostgres extends AuthDao { }
